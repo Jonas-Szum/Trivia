9a1b6bc7f6fd38551f34d61ba0c55c509b85f480
Jonas-Szum <46059570+Jonas-Szum@users.noreply.github.com>
2019-05-02 13:48:24 -0500

    Updated what the server sends

--- Server/src/Server/Server.java
+++ Server/src/Server/Server.java
@@ -17,18 +17,19 @@ public class Server {
     private ServerSocket server_socket;
     boolean active = false;
     boolean validPort = false;
-    int p1Score = 0;
-    int p2Score = 0;
-    volatile ArrayList<Integer> playerScores;	//new to this version
-    volatile ArrayList<String>  playerMoves;	//new to this version
-    String p1Move;
-    String p2Move;
+    volatile ArrayList<Integer> playerScores;
+    volatile ArrayList<String>  playerMoves;
     Consumer<Serializable> callback;
     boolean gameOver = false;
     String winner;
     int numPlayers = 0;
-    int maxPlayers = 3;
+    int maxPlayers = 4;
     int clientIDs = 1;
+    volatile ArrayList<Integer> answerOrder;
+    volatile boolean calculated = false;
+    volatile QnA myTrivia;
+    volatile String question = "";
+    volatile ArrayList<String> answers;
 
     //store the active connections
     ArrayList<Connection> connectionList = new ArrayList<Connection>();
@@ -41,6 +42,10 @@ public class Server {
         {
             playerScores.add(0);
             playerMoves.add("");
+            myTrivia = new QnA();
+            myTrivia.roll_question();
+            question = myTrivia.get_question();
+            answers = myTrivia.get_answers();
         }
     }
 
@@ -135,35 +140,22 @@ public class Server {
                     ObjectInputStream in = new ObjectInputStream(s.getInputStream());
                     this.output = out;
                     this.input = in;
-                    playerID = numPlayers; //starts at 0, goes up to numPlayers-1
                     numPlayers++;
                     updateClients();
                     callback.accept("Found connection");
 
                     while(true) {
-                        Serializable challenge = (Serializable) in.readObject(); //read someone challenging someone else, or accepting someone else's challenge
-                        if(alreadyInGame == false) //user is challenging someone else
-                        {
-                            alreadyInGame = true;
-                            currentOpponent = (int)challenge; //set current opponent the the user that you challenged
-
-                            connectionList.get(currentOpponent).alreadyInGame = true;  //set opponent's current opponent to you
-                            connectionList.get(currentOpponent).currentOpponent = playerID; //after that, get ready to read the user's moves
-                            updateClients(); //update the clients GUIs to reflect who's in a game
-                            Serializable data = (Serializable) in.readObject();
-                            this.ClientMove = (String) data;
-                            this.madeMove = true;
-                            //notify other user that they're in a game
-                        }
-                        else //user is already in game, and is sending their move
-                        {
-                            Serializable data = (Serializable) in.readObject();
-                            this.ClientMove = (String) data;
-                            this.madeMove = true;
-                        }
-
-
-                        calculateRound(playerID, currentOpponent); //if you are not versing anyone, currentOpponent is -1, go to calculateRound to see why
+                    	out.writeObject(question);
+                    	out.writeObject(answers);
+                        //send the question, answer combo
+                    	//wait to recieve answer
+                    	Serializable playerAnswer = (Serializable)in.readObject();
+                    	calculated = false; //after first player answers again, calculated is false
+                    	answerOrder.add(playerID);
+                    	if(answerOrder.size() == 4)
+                    		calculateRound(); //if you are not versing anyone, currentOpponent is -1, go to calculateRound to see why
+                    	else
+                    		while(calculated == false) {}
                     }
                 } //end of if not null
 
@@ -175,7 +167,6 @@ public class Server {
                 //numPlayers = -1; //holder for if we were to lose a player, then we are going to end game
                 numPlayers = -1; //new to this version
                 callback.accept("Not enough players to play.");
-                //BELOW NEEDS TO BE CHANGED
                 updateClients();
 
             } //end of catch
@@ -183,177 +174,14 @@ public class Server {
     } //end of connection method
 
     //calculates who won the round, and the game
-    private synchronized void calculateRound(int player1, int player2) { //now takes the ID's of the two players, so it knows who to check
-        if (	player2 != -1 //if player2 is -1, then there is not a game currently going on, this if statement shorts out
-                && (connectionList.get(player1).playerScore != 3 && connectionList.get(player2).playerScore != 3)
-                && numPlayers >1) {
-            //if anyone has not made a move yet, do nothing
-            if (!connectionList.get(player1).madeMove || !connectionList.get(player2).madeMove) {
-                return;
-            }
-            else if (connectionList.get(player1).ClientMove.equals("Rock")) {
-                switch (connectionList.get(player2).ClientMove) {
-                    case "Rock":
-                        break;
-                    case "Paper":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Scissors":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Lizard":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Spock":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                }
-            }
-            else if (connectionList.get(player1).ClientMove.equals("Paper")) {
-                switch (connectionList.get(player2).ClientMove) {
-                    case "Rock":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Paper":
-                        break;
-                    case "Scissors":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Lizard":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Spock":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                }
-            }
-            else if (connectionList.get(player1).ClientMove.equals("Scissors")) {
-                switch (connectionList.get(player2).ClientMove) {
-                    case "Rock":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Paper":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Scissors":
-                        break;
-                    case "Lizard":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Spock":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                }
-            }
-            else if (connectionList.get(player1).ClientMove.equals("Lizard")) {
-                switch (connectionList.get(player2).ClientMove) {
-                    case "Rock":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Paper":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Scissors":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Lizard":
-                        break;
-                    case "Spock":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                }
-            }
-            else if (connectionList.get(player1).ClientMove.equals("Spock")) {
-                switch (connectionList.get(player2).ClientMove) {
-                    case "Rock":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Paper":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Scissors":
-                        connectionList.get(player1).playerScore++;
-                        break;
-                    case "Lizard":
-                        connectionList.get(player2).playerScore++;
-                        break;
-                    case "Spock":
-                        break;
-                }
-            }
-
-            //reset so they need to make moves again
-            connectionList.get(player1).madeMove = false;
-            connectionList.get(player2).madeMove = false;
-
-            //update the client's version of the scores
-            p1Score = connectionList.get(player1).playerScore;
-            p2Score = connectionList.get(player2).playerScore;
-            p1Move = connectionList.get(player1).ClientMove;
-            p2Move = connectionList.get(player2).ClientMove;
-
-            callback.accept("Moves have been updated");
-
-            if (connectionList.get(player1).playerScore == 3) {
-                winner = "Player " + String.valueOf(player1);
-                gameOver = true;
-            }
-
-            else if (connectionList.get(player2).playerScore == 3) {
-                winner = "Player " + String.valueOf(player2);
-                gameOver = true;
-            }
-
-
-            updateClients(); //server knows which players to send data to this way
-        } //end of checking if 6 case statement
-
-        else if (connectionList.get(player1).playerScore == 3) {
-            if (connectionList.get(player1).ClientMove.equals("Play")) {
-
-                //when you click play again, you elicit this so everyone is available to play
-                for (Connection conn : connectionList) {
-                    conn.alreadyInGame = false;
-                }
-
-                connectionList.get(player1).playerScore = 0;
-                connectionList.get(player2).playerScore = 0;
-
-                connectionList.get(player1).madeMove = false;
-                connectionList.get(player2).madeMove = false;
-                playerScores.set(player1, 0);
-                playerScores.set(player2, 0);
-                ///p1Score = 0;
-                //p2Score = 0;
-                winner = "Playing again";
-                callback.accept("replay");
-            }
-            else {
-                System.out.println("Winner is already P1");
-            }
-        }
-
-        else if (connectionList.get(player2).playerScore == 3) {
-            if (connectionList.get(player2).ClientMove.equals("Play")) {
-                connectionList.get(player1).playerScore = 0;
-                connectionList.get(player2).playerScore = 0;
-
-                connectionList.get(player1).madeMove = false;
-                connectionList.get(player2).madeMove = false;
-
-                playerScores.set(player1, 0);
-                playerScores.set(player2, 0);
-                //p1Score = 0;
-                //p2Score = 0;
-
-                winner = "Playing again";
-                callback.accept("replay");
-            }
-            else {
-                System.out.println("Winner is already P2");
-            }
-        }
-
+    private synchronized void calculateRound() {
+    //send information to all 4 players
+    
+    myTrivia.roll_question();
+    question = myTrivia.get_question();
+    answers = myTrivia.get_answers();
+    answerOrder.clear();
+    calculated = true;
     } //end of calculateround
 
 
